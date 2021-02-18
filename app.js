6c6bf37e30ab8ec8d4116d3601f2ce4be164f9b2
capn <jamesgreen.311@gmail.com>
2021-02-18 15:10:16 -0500

    show first email address

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function getEmailList() {
     fetch(url)
         .then(d => d.json())
         .then(d => {
-            document.getElementById("app").textContent = d[0].status;
+            document.getElementById("app").textContent = d[0].data[0].email;
         });    
 }
 function addRow() {
