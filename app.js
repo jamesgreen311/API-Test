a7a09ca4bb8761a348c462e67f094312f471f952
capn <jamesgreen.311@gmail.com>
2021-02-19 13:52:52 -0500

    Retrieve list, Copy to Clipboard

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ function getEmailList() {
     fetch(url)
         .then(d => d.json())
         .then(d => {
-            document.getElementById("app").textContent = d[0].data[2].email;
+            showList(d[0].data);
         });    
 }
+
+function showList(arr) {
+    const table = document.getElementById("emailTable");
+    arr.forEach(el => {
+        table.insertRow(-1).insertCell(0).innerHTML = el.email;
+    });
+}
+
 function addRow() {
     const url = "https://script.google.com/macros/s/AKfycbw7LsL3ASCbX82jmKa0K1_P66Lz8mBTqh5LLJbpxktF4GR4shm8qBLYig/exec";
     fetch(url,{
@@ -20,4 +28,21 @@ function addRow() {
     }); 
 }
 
+function copyToClipboard() {
+    const listTable = document.getElementById("emailTable").tBodies[0].rows[0].cells[0].innerText;
+    const rows = document.getElementById("emailTable").tBodies[0].rows;
+    const textarea = document.createElement('textarea');
+    textarea.setAttribute('readonly', '');
+    textarea.style.position = 'absolute';
+    textarea.style.left = '-9999px';
+    for (i=0; i<rows.length; i++) {
+        textarea.value += `${rows[i].textContent},`;
+    }
+    document.body.appendChild(textarea);
+    
+    textarea.select();
+    document.execCommand('copy');
+}
+
 document.getElementById("btn").addEventListener("click", getEmailList);
+document.getElementById("copyBtn").addEventListener("click", copyToClipboard);
